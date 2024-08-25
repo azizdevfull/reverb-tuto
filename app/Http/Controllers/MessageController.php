ba25b2d1af67139b50b917a998669d735e7c201e
Isroilov Azizbek <aziz16110904@gmail.com>
2024-08-25 20:15:30 +0500

    completed

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\MessageSentEvent;
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $message = Message::create(['text' => $request->message]);
+
+        broadcast(new MessageSentEvent($message));
+
+        return response()->json([
+            'message' => "Message {$message->id} saved",
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
